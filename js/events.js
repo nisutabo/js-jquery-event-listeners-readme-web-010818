602b3f561a1139e7165327d212b17dd1455147a5
github username <github email address>
2018-03-02 13:56:29 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,63 @@
 $(document).ready(function(){
 
 // call functions here
+  // $('h1').on("eventname", function(){
+  //
+  // });
+
+
+
+  // $("form").on("submit", function (){
+  //   if ($("input:first" ).val() === "correct"){
+  //     alert('your form is going to be submitted now');
+  //     return;
+  //   }
+  //   alert("you entered the wrong value");
+  //   return ;
+  // });
+
+  $('p').on("click", () =>{
+  return  getIt()
+  });
+
+  $("img").on("load", () =>{
+    return frameIt()
+  });
+
+  // $('#typing').on('keydown', (e) =>{
+  //   return pressIt(e)
+  // });
+
+ pressIt()
+
+ submitIt()
 
 });
+
+function getIt(){
+  return alert("Hey!");
+}
+
+function frameIt(){
+  return $('img').addClass('tasty')
+}
+
+function pressIt(){
+  $('#typing').on('keydown', function(e) {
+    if (e.which === 71){
+      alert("You have pressed the G key.")
+    }
+  })
+}
+
+// function pressIt(e){
+//   if (e.which === 71){
+//     return alert("You pressed 'g'!")
+//   }
+
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
